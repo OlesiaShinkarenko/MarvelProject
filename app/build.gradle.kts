a6863e763d8f7f257b75195eaf3db9ffd49b293e
Leci <leca.deca@mail.ru>
2024-02-28 20:37:22 +0600

    [+] Added necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id(Plugins.Android.application)
+    id(Plugins.Android.jetbrains)
 }
 
 android {
@@ -51,19 +51,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Dependencies.AndroidX.core)
+    implementation(Dependencies.AndroidX.lifecycle)
+    implementation(Dependencies.AndroidX.activity)
+    implementation(platform(Dependencies.AndroidX.compose_bom))
+    implementation(Dependencies.AndroidX.compose_ui)
+    implementation(Dependencies.AndroidX.compose_ui_graphics)
+    implementation(Dependencies.AndroidX.compose_ui_tooling)
+    implementation(Dependencies.AndroidX.material3)
+    implementation(Dependencies.AndroidX.navigation)
+    implementation(Dependencies.Coil.coil)
 }
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,26 @@
+object Dependencies{
+    object Version{
+        const val core = "1.12.0"
+        const val lifecycle = "2.7.0"
+        const val activity = "1.8.2"
+        const val compose_bom = "2023.08.00"
+        const val navigation = "2.7.7"
+        const val coil = "2.6.0"
+    }
+
+    object AndroidX{
+        const val core = "androidx.core:core-ktx:${Version.core}"
+        const val lifecycle = "androidx.lifecycle:lifecycle-runtime-ktx:${Version.lifecycle}"
+        const val activity = "androidx.activity:activity-compose:${Version.activity}"
+        const val compose_bom = "androidx.compose:compose-bom:${Version.compose_bom}"
+        const val compose_ui = "androidx.compose.ui:ui"
+        const val compose_ui_graphics = "androidx.compose.ui:ui-graphics"
+        const val compose_ui_tooling = "androidx.compose.ui:ui-tooling-preview"
+        const val material3 = "androidx.compose.material3:material3"
+        const val navigation = "androidx.navigation:navigation-compose:${Version.navigation}"
+    }
+
+    object Coil{
+        const val coil = "io.coil-kt:coil-compose:${Version.coil}"
+    }
+}
