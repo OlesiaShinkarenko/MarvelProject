ee3faf496fe16da23340d4c11250ce0fc727d94f
Leci <leca.deca@mail.ru>
2024-02-28 22:24:02 +0600

    [+] Added font xml files

---
+++ app/src/main/java/effective/office/marvelproject/ui/theme/Fonts.kt
@@ -0,0 +1,11 @@
+package effective.office.marvelproject.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import effective.office.marvelproject.R
+
+internal val FontFamily = FontFamily(
+    Font(R.font.tt_hoves_pro_trial_bold_font, weight = FontWeight.Bold),
+    Font(R.font.tt_hoves_pro_trial_medium_font, weight = FontWeight.Medium)
+)
